8091c82b86f7d8fc6d2f43807513e8da111eda7f
Hack Reactor Students <accounts@hackreactor.com>
2015-03-05 16:31:03 -0800

    Implement MongoDB in replacement of SQL

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,10 +6,6 @@ module.exports = function(grunt) {
     concat: {
       dist:{
         src: [
-          'public/lib/handlebars.js',
-          'public/lib/underscore.js',
-          'public/lib/jquery.js',
-          'public/lib/backbone.js',
           'public/client/*.js'
         ],
         dest: 'public/dist/production.js'
