d6dccfb8419c3ae8bd354a028f75474a36842b7f
Hack Reactor Students <accounts@hackreactor.com>
2015-03-05 20:11:56 -0800

    Implement link creation

--- app/models/link.js
+++ app/models/link.js
@@ -5,22 +5,9 @@ var mongoose = require('mongoose');
 var Url = mongoose.model('url', db.urlSchema);
 module.exports = Url;
 
-// var Link = db.model('Link', urlSchema,
-//   {
-//   tableName: 'urls',
-//   hasTimestamps: true,
-//   defaults: {
-//     visits: 0
-//   },
-//   initialize: function(){
-//     this.on('creating', function(model, attrs, options){
-//       var shasum = crypto.createHash('sha1');
-//       shasum.update(model.get('url'));
-//       model.set('code', shasum.digest('hex').slice(0, 5));
-//     });
-//   }
-// });
-
-
-
-// module.exports = Link;
+db.urlSchema.pre('save', function(next){
+  var shasum = crypto.createHash('sha1');
+  shasum.update(this.url);
+  this.code = shasum.digest('hex').slice(0, 5);
+  next();
+});
