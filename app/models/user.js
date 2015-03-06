3ce2043ea09c990ee186ff8b80298f93dbb48baa
Hack Reactor Students <accounts@hackreactor.com>
2015-03-05 19:25:36 -0800

    Implement login feature
    - Debug sign up feature

--- app/models/user.js
+++ app/models/user.js
@@ -20,25 +20,3 @@ db.usersSchema.pre('save', function(next){
       next();
   });
 });
-
-// var User = db.Model.extend({
-  // tableName: 'users',
-  // hasTimestamps: true,
-  // initialize: function(){
-  //   this.on('creating', this.hashPassword);
-  // },
-  // comparePassword: function(attemptedPassword, callback) {
-  //   bcrypt.compare(attemptedPassword, this.get('password'), function(err, isMatch) {
-  //     callback(isMatch);
-  //   });
-  // },
-  // hashPassword: function(){
-  //   var cipher = Promise.promisify(bcrypt.hash);
-  //   return cipher(this.get('hashPasswordsword'), null, null).bind(this)
-  //     .then(function(hash) {
-  //       this.set('password', hash);
-  //     });
-  // }
-// });
-
-// module.exports = User;
